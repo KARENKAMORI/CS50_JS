65ffb0ac03cf61d72798e177fd7077c49a29e573
KARENKAMORI <kamorikaren2019@gmail.com>
2024-04-18 23:12:20 +0300

    Javascript code that changes text in a HTML file from normal to bold and vice versa

--- 15select.js
+++ 15select.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('select').onchange = function(){
+        // use fontWeight instead of font-weight, Javascript uses camelCase
         document.querySelector('#hello').style.fontWeight = this.value;
     }
 });
