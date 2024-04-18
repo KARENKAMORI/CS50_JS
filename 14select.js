2bb28b68edb21eacb438dcacb3db57c9b424ca3e
KARENKAMORI <kamorikaren2019@gmail.com>
2024-04-18 22:27:07 +0300

    Usecase for 'this' keyword

--- 14select.js
+++ 14select.js
@@ -2,4 +2,5 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('select').onchange = function(){
         document.querySelector('#hello').style.color = this.value;
     }
+    
 });
