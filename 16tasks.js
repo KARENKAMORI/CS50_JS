15106b759643c7a69dabaf88e792faded8791e2b
KARENKAMORI <kamorikaren2019@gmail.com>
2024-04-20 12:20:36 +0300

    Javascript code that creates li elements into the ul tag in a connected HTML file

--- 16tasks.js
+++ 16tasks.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
+    // By default, the submit button should be disabled when you first load the page
+    document.querySelector('#submit').disabled = true;
+
+    // Enabling the key when user starts typing
+    document.querySelector('#task').onkeyup = () => {
+        // checks whether there is input just incase the user types in and then deletes input, it shouldn't allow empty inputs
+        if (document.querySelector('#task').value.length > 0){
+            document.querySelector('#submit').disabled = false;
+        } else{
+            document.querySelector('#submit').disabled = true;
+        }
+    }
+
     document.querySelector('form').onsubmit = () => {
         const task = document.querySelector('#task').value;
         // create a new element
@@ -9,6 +22,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
         // clearing up the input field
         document.querySelector('#task').value = '';
+
+        // disable the submit button once you have submitted your input
+        document.querySelector('#submit').disabled = true;
 
         // stop form from submitting
         return false;
